92cc2f53cf9336733989a26e7569fc320c1bd024
Andre Dal Santos <andredalsantos@gmail.com>
2020-08-27 17:36:59 -0300

    challenge 06 ok

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,66 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type not valid.');
+    }
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (total < value && type === 'outcome') {
+      throw new AppError('Balance not enough to this transaction.');
+    }
+
+    const categoriesRepository = getRepository(Category);
+
+    const checkCategoryExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    let categoryId = '';
+
+    if (checkCategoryExists) {
+      categoryId = await categoriesRepository.getId(checkCategoryExists);
+    } else {
+      const newCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(newCategory);
+
+      categoryId = await categoriesRepository.getId(newCategory);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryId,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
